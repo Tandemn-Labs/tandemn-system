808913c05f2edf86925605b8189c6775400ac40b
Hetarth Chopra <choprahetarth@gmail.com>
2025-11-17 19:19:47 -0800

    [errors] fixed

--- storage/backends/base.py
+++ storage/backends/base.py
@@ -80,65 +80,65 @@ class StorageBackend(ABC):
         return f"users/{user}/"
 
 
-# ============================== PreSigned Upload and Download Features ==============================
+    # ============================== PreSigned Upload and Download Features ==============================
 
-# The point we are looking at is to create multipart uploads by
-# creating presigned URLs for each chunk of the total payload.
-# The client will take care of the multipart lifecyle and we will just return the presigned URLs.
-# However such kind of mechanism is NOT NEEDED for downloada
-@abstractmethod
-async def presigned_upload(self, remote_path:str, user:str, expires_seconds:int=600) -> dict: 
-    """
-    Returns a presigned URL for a SINGLE CHUNK of the total payload.
-    return {"type": "single", "url":".....", "method":"put", "headers":{"Content-Type":"application/octet-stream"}}
-    """
-    pass
+    # The point we are looking at is to create multipart uploads by
+    # creating presigned URLs for each chunk of the total payload.
+    # The client will take care of the multipart lifecyle and we will just return the presigned URLs.
+    # However such kind of mechanism is NOT NEEDED for downloada
+    @abstractmethod
+    async def presigned_upload(self, remote_path:str, user:str, expires_seconds:int=600) -> dict: 
+        """
+        Returns a presigned URL for a SINGLE CHUNK of the total payload.
+        return {"type": "single", "url":".....", "method":"put", "headers":{"Content-Type":"application/octet-stream"}}
+        """
+        pass
 
-@abstractmethod
-async def presigned_download(self, remote_path:str, user:str, expires_seconds:int=600) -> dict: 
-    """
-    Returns a presigned URL for downloading the total payload.
-    return { "url":".....", "method":"get", "headers":{"Accept":"application/octet-stream"}}
-    """
-    pass
+    @abstractmethod
+    async def presigned_download(self, remote_path:str, user:str, expires_seconds:int=600) -> dict: 
+        """
+        Returns a presigned URL for downloading the total payload.
+        return { "url":".....", "method":"get", "headers":{"Accept":"application/octet-stream"}}
+        """
+        pass
 
-# multipart lifecycle
-@abstractmethod
-async def multipart_upload_start(self, remote_path:str, user:str) -> dict:
-    """
-    This basically marks the start of the multipart upload and just returns the upload Id.
-    When you start uploading on AWS S3, in multiparts, you get an UploadID which
-    acts as a uniqe identifier for the multipart upload.
-    return {"upload_id":".....", "key":"....."}
-    """
-    pass
+    # multipart lifecycle
+    @abstractmethod
+    async def multipart_upload_start(self, remote_path:str, user:str) -> dict:
+        """
+        This basically marks the start of the multipart upload and just returns the upload Id.
+        When you start uploading on AWS S3, in multiparts, you get an UploadID which
+        acts as a uniqe identifier for the multipart upload.
+        return {"upload_id":".....", "key":"....."}
+        """
+        pass
 
-@abstractmethod
-async def multipart_sign_part(self, upload_id:str, user:str, remote_path:str, part_number:int, expires_seconds:int=600) -> dict:
-    """
-    Everytime we have to upload a file in a multipart chunk, we need to sign the part's URL.
-    This will be called again and again for each part of the file we are uploading. 
-    return {"url":".....", "method":"put", "headers":{"Content-Type":"application/octet-stream"}}
-    """
-    pass
+    @abstractmethod
+    async def multipart_sign_part(self, upload_id:str, user:str, remote_path:str, part_number:int, expires_seconds:int=600) -> dict:
+        """
+        Everytime we have to upload a file in a multipart chunk, we need to sign the part's URL.
+        This will be called again and again for each part of the file we are uploading. 
+        return {"url":".....", "method":"put", "headers":{"Content-Type":"application/octet-stream"}}
+        """
+        pass
 
-@abstractmethod
-async def multipart_complete(self, remote_path:str, user:str, upload_id:str, part_ids:list[str]) -> dict:
-    """
-    This is the final step in the multipart upload.
-    This just returns the final URI of the uploaded file.
-    """
-    pass
+    @abstractmethod
+    async def multipart_complete(self, remote_path:str, user:str, upload_id:str, part_ids:list[str]) -> dict:
+        """
+        This is the final step in the multipart upload.
+        This just returns the final URI of the uploaded file.
+        """
+        pass
 
-@abstractmethod
-async def multipart_abort(
-    self,
-    remote_path: str,
-    user: str,
-    upload_id: str,
-) -> None:
-    """
-    This just aborts the multipart upload.
-    return True if successful
-    """
-    pass
+    @abstractmethod
+    async def multipart_abort(
+        self,
+        remote_path: str,
+        user: str,
+        upload_id: str,
+    ) -> None:
+        """
+        This just aborts the multipart upload.
+        return True if successful
+        """
+        pass
