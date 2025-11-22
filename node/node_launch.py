5f7bc5bfa251f874b496e8322350ee20735b8817
Orange Ng <ngquanhao@outlook.com>
2025-11-21 17:13:21 -0800

    Added ZMQ for head node to worker node comms

--- node/node_launch.py
+++ node/node_launch.py
@@ -3,7 +3,10 @@ from dataclasses import dataclass
 import logging
 import os
 import subprocess
+import time
 from typing import List
+import zmq
+import msgpack
 
 @dataclass
 class GlobalConfig:
@@ -12,6 +15,8 @@ class GlobalConfig:
 @dataclass
 class LaunchConfig:
     ray_head_port: int = 54321
+    chain_comms_port: int = 54322
+    ray_setup_timeout: int = 60
 
 config = GlobalConfig()
 launch_config = LaunchConfig()
@@ -19,13 +24,14 @@ launch_config = LaunchConfig()
 @dataclass
 class LaunchOptions:
     node_list: List[str]
+    node_addrs: List[str]
     engine: str
-    head_address: str
     model: str
     tp_size: int
     pp_size: int
 
-logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 # `options` is a dictionary of launch options
 def launch_engine(options):
@@ -33,32 +39,63 @@ def launch_engine(options):
     opt = options
 
     if config.hostname not in opt.node_list:
-        logging.error("Hostname %s is not included in node_list", config.hostname)
+        logger.error("Hostname %s is not included in node_list", config.hostname)
         return
     
     match opt.engine:
         case "vllm":
             launch_vllm(options)
         case _:
-            logging.error("Engine %s is not supported right now", opt.engine)
+            logger.error("Engine %s is not supported right now", opt.engine)
 
 # Right now we use Ray to do multi-node vLLM, probably will change in the future
-def launch_vllm(options):
+def launch_vllm(options: LaunchConfig):
 
-    lc, opt = launch_config, options
+    global launch_config, config
+    cfg, lc, opt = config, launch_config, options
 
     # Launch Ray cluster
-    if config.hostname == options.node_list[0]:
+    ctx = zmq.Context()
+    if config.hostname == options.node_list[0]: # Logic for head node
         ray_head = ["ray", "start", "--head", f"--port={lc.ray_head_port}"]
         subprocess.run(ray_head)
-    else:
-        ray_worker = ["ray", "start", f"--address={opt.head_address}:{lc.ray_head_port}"]
-        subprocess.run(ray_worker)
-    
-    # TODO: Does Ray let you programmatically manage cluster?
 
+        listen = ctx.socket(zmq.REP)
+        listen.bind(f"tcp://*:{lc.chain_comms_port}")
+        confirmations = []
+        start_time = time.monotonic()
+
+        while (time.monotonic() - start_time) < lc.ray_setup_timeout and len(confirmations) < (len(opt.node_list) - 1):
+            try:
+                msg = listen.recv(flags=zmq.NOBLOCK)
+                results = msgpack.unpackb(msg)
+                if results["ray_status"]:
+                    confirmations.append(results["hostname"])
+
+            except zmq.ZMQError:
+                # Ignore ZMQError since it's triggered by no message found on non-blocking
+                continue
+        
+        # Check ray up or timeout
+        if len(confirmations) != (len(opt.node_list) - 1):
+            logger.error("Timeout of %d seconds reached. Ray cluster not up", lc.ray_setup_timeout)
+            logger.error("List of nodes with confirmation: %s", confirmations)
+            return
+        else:
+            logger.info("Ray cluster initialized. Confirmations: %s", confirmations)
+
+    else: # Logic for non-head nodes
+        ray_worker = ["ray", "start", f"--address={opt.node_addrs[0]}:{lc.ray_head_port}"]
+        subprocess.run(ray_worker)
+
+        send_sock = ctx.socket(zmq.REQ)
+        send_sock.connect(f"tcp://{opt.node_addrs[0]}:{lc.chain_comms_port}")
+        status = {"ray_status": 1, "hostname": cfg.hostname}
+        status_bytes = msgpack.packb(status)
+        send_sock.send(status_bytes, flags=zmq.NOBLOCK)
+    
     # Only first node in chain launch vllm
-    if config.hostname != options.node_list[0]:
+    if cfg.hostname != options.node_list[0]:
         return
 
     #? Will probably move to a new file?
@@ -73,7 +110,7 @@ def launch_vllm(options):
 
     # Just for testing prompts
     prompts = [
-        "Tell me more about the idea of irreducible complexity",
+        "Tell me more about the idea of irreducible complexity!",
         "What is the difference between a spreadsheet and a database?",
         "What are your thoughts on the myth of Cupid and Psyche?"
     ]
@@ -81,6 +118,18 @@ def launch_vllm(options):
     outputs = llm.generate(prompts)
     print(outputs)
 
+# # Called by the head node
+# def teardown_ray(options: LaunchOptions):
+
+#     global launch_config
+#     opt, lc = options, launch_config
+
+#     ctx = zmq.Context()
+#     for addr in opt.node_addrs[1:]:
+#         kill_sock = ctx.socket(zmq.REQ)
+#         kill_sock.connect(f"tcp://{addr}:{lc.chain_comms_port}")
+
+#     pass
 
 def setup():
     global config, launch_config
@@ -95,9 +144,9 @@ def main():
     setup()
 
     opt = LaunchOptions(
-        node_list = [],
+        node_list = ["test1", "test2"],
+        node_addrs = ["",""],
         engine = "vllm",
-        head_address = "",
         model = "facebook/opt-125m",
         tp_size = 4,
         pp_size = 2
