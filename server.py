9cd4242a7e1f03d1669cadf8e5b8329056837b1c
Orange Ng <ngquanhao@outlook.com>
2026-01-07 17:37:15 -0800

    Added yamls and yaml parse and replace

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
+import uuid
 from fastapi import FastAPI
-from models.models import UserRequest
+from models.requests import BatchedRequest
+from models.resources import MagicOutput
+from utils.yaml import update_yaml_file
 
 # To run the server:
 # uvicorn server:app --reload
@@ -12,18 +15,41 @@ app = FastAPI(
 )
 
 
-@app.post("/request")
-async def submit_request(request: UserRequest):
+@app.post("/submit/batch")
+async def submit_batch(request: BatchedRequest):
     """
-    Submit a job request.
-    
-    Receives a UserRequest with job configuration and returns
+    Submit a batched inference job request.
+
+    Receives a BatchedRequest with job configuration and returns
     confirmation of receipt.
     """
-    print(type(request))
-    print(request)
+
+    launch_config = real_magic(request)
+
+    match launch_config.engine:
+        case "vllm":
+            pass
+
+
+async def sp_launch_vllm(request: BatchedRequest, config: MagicOutput):
+    replace_dict = {
+        "name": config.decision_id,
+        "num_nodes": config.num_nodes,
+        "resources.instance_type": config.instances,
+    }
+    update_yaml_file("yamls/vllm.yaml", replace_dict, "temp/output.yaml")
+
+    pass
 
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=26336)
+
+
+### magic.py placeholder
+
+
+def real_magic(request: BatchedRequest) -> MagicOutput:
+    return MagicOutput("mo" + uuid.uuid4(), "vllm", "g4dn.12xlarge", 4, 4, 4)
