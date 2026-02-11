fd2c5c2fd0a71b40b91e181ce9b574865a9cfc8b
Orange Ng <ngquanhao@outlook.com>
2026-02-10 23:44:09 -0800

    Minor fix to example model, Fix instance type choosing bug

--- models/resources.py
+++ models/resources.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 from pydantic import BaseModel
 
+
 class MagicOutput(BaseModel):
     decision_id: str
     engine: str
     instance_type: str
+    num_inst: int
     tp_size: int
     pp_size: int
     replicas: int
